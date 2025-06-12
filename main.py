b5be5d234ec65139e59ce526c03edbfbc0a6df4b
cry0404 <1527653155@qq.com>
2025-06-12 22:56:07 +0800

    feat: 简单的函数调用支持

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import argparse
 from google.genai import types
 from system_prompt import system_prompt
+from declare_function import schema_get_files_info
 load_dotenv()
 API_KEY = os.getenv("GEMINI_API_KEY")
 
@@ -26,19 +27,33 @@ messages = [
     ]
   )
 ]
-
+available_functions = types.Tool(
+  function_declarations = [
+    schema_get_files_info,
+  ]
+)
 client = genai.Client(api_key=API_KEY)
+
+#不要仅仅打印 generate_content 响应的 .text 属性，
+# 还要检查 .function_calls 属性。如果 LLM 调用了函数，请打印函数名称和参数：
+
+
+
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
     contents= messages,
     config=types.GenerateContentConfig(
+      tools=[available_functions],
       system_instruction=system_prompt
     )
 )
-prompt_token_count = response.usage_metadata.prompt_token_count
-candidates_token_count = response.usage_metadata.candidates_token_count
-if verbose:
-  print(f"User prompt: {user_prompt}")
-  print(f"Prompt tokens: {prompt_token_count}")
-  print(f"Response tokens: {candidates_token_count}")
-print(response.text)
+if response.function_calls: #如果LLM调用了函数，则打印函数名称和参数
+  print(f"Calling function: {response.function_calls[0].name}({response.function_calls[0].args})")
+else:
+  prompt_token_count = response.usage_metadata.prompt_token_count
+  candidates_token_count = response.usage_metadata.candidates_token_count
+  if verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {prompt_token_count}")
+    print(f"Response tokens: {candidates_token_count}")
+  print(response.text)
